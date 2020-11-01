bd12e38c74b21049ddbf874d60b1d9e8c167e7a4
Aleksandr784 <digiprotekt@gmale.com>
2020-11-01 21:59:32 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header >
+       <a href="/">React Shoping Cart</a>
+      </header>
+      <main>
+        Product List
+      </main>
+      <footer>
+        All rights reserved
+      </footer>
+    </div>
+  );
+}
+
+export default App;
